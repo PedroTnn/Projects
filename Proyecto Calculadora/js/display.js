2b5be1051f3317559fb8f6f3b12d7040ed6f9c16
Pedro T <pedrotnns@gmail.com>
2022-07-18 14:54:17 -0600

    Progress Display.js

--- Proyecto Calculadora/js/display.js
+++ Proyecto Calculadora/js/display.js
@@ -3,17 +3,43 @@ class Display{
         this.displayValueActualy = displayValueActualy;
         this.displayValueBefore = displayValueBefore;
         this.calculator = new Calculator();
+        this.typeOperation = undefined;
         this.valueActualy='';
         this.valueBefore = '';
+
     }
     addNumber(numero){
-        this.valueActualy = this.valueActualy + numero;
+        if(numero === '.' && this.valueActualy.includes ('.'))  return
+        this.valueActualy = this.valueActualy.toString() + numero.toString();
         this.printValue();
     }
 
     printValue(){
         this.displayValueActualy.textContent = this.valueActualy;
         this.displayValueBefore.textContent = this.valueBefore;
-        /*Ando triste vamos a chupar */
+     
+    }
+
+    delete(){
+        this.valueActualy = this.valueActualy.toString().slice(0,-1);
+        this.printValue();
+    }
+    deleteDisplay(){
+        this.valueActualy = '';
+        this.valueBefore = '';
+        this.typeOperation = undefined;
+        this.printValue();
+    }
+
+    calculate(){
+        const valueBefore = parseFloat(this.valueBefore);
+        const valueActualy = parseFloat(this.valueBefore);
+        if(isNaN(valueActualy) || isNaN(this.valueBefore)) return 
+        this.valueActualy = this.calculator[this.typeOperation](valueBefore, valueActualy);
+    }
+    compute(){
+        this.typeOperation !== 'same' && this.calculate();
+        this.typeOperation = tipo;
+        this.valueBefore = this.valueActualy || this.valueBefore;
     }
 }
